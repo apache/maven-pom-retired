ddb657b5558b47e2bbb919aa606c269ecda14c23
Trygve Laugstol <trygvis@apache.org>
2005-04-21 13:41:17 +0000

    o Improving readability.
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@164056 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 m2 -N install
-(cd continuum-model && m2 install)
-(cd continuum-core && m2 install)
-(cd continuum-web && m2 install)
-(cd continuum-xmlroc && m2 install)
-(cd continuum-plexus-application && sh build.sh)
+( cd continuum-coo && m2 install )
+( cd continuum-core && m2 install )
+( cd continuum-web && m2 install )
+( cd continuum-xmlrpc && m2 install )
+( cd continuum-plexus-application && sh build.sh )
