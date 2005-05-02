e69f7493fa78a1a140b778d02001696e1001f74c
Trygve Laugstol <trygvis@apache.org>
2005-05-02 14:16:38 +0000

    o Build as much as possible in a single reactor.
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@165635 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 set -e
 set -x
 
+includes=\
+continuum-model/pom.xml,\
+continuum-core/pom.xml,\
+continuum-web/pom.xml,\
+continuum-xmlrpc/pom.xml
+
 m2 clean:clean
 m2 -N install
-( cd continuum-model && m2 install )
-( cd continuum-core && m2 install )
-( cd continuum-web && m2 install )
-( cd continuum-xmlrpc && m2 install )
+m2 -r -Dmaven.reactor.includes="$includes" install
+
 ( cd continuum-plexus-application && sh build.sh )
