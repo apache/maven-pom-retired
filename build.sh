54a8f715bd595d2e4cc992eace3d1dfc7aec45dc
Trygve Laugstol <trygvis@apache.org>
2005-05-04 12:59:48 +0000

    o Making sure everything is cleaned, not only the <modules>.
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@168120 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ continuum-core/pom.xml,\
 continuum-web/pom.xml,\
 continuum-xmlrpc/pom.xml
 
-m2 clean:clean
+m2 -r -Dmaven.reactor.includes=*/pom.xml clean:clean
 m2 -N install
 m2 -r -Dmaven.reactor.includes="$includes" install
 
