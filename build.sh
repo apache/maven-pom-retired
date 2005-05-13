160999fab45d24e8cf089947113b5e2ab0be3e97
Trygve Laugstol <trygvis@apache.org>
2005-05-13 11:05:46 +0000

    o Oops, forgot this one.
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@170006 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ set -e
 set -x
 
 includes=\
-continuum-model/pom.xml,\
+continuum-api/pom.xml,\
 continuum-core/pom.xml,\
+continuum-model/pom.xml,\
 continuum-web/pom.xml,\
 continuum-xmlrpc/pom.xml
 
