c95a7929ddfa12458ade8a3843fd6a1640bc0f1c
Trygve Laugstol <trygvis@apache.org>
2005-06-01 10:57:58 +0000

    o Making the build.sh tee the output to build.log.   Thanks to Jerome Lacoste for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@179373 13f79535-47bb-0310-9956-ffa450edef68

--- README.txt
+++ README.txt
@@ -3,15 +3,15 @@ Building
 
 Run:
 
-  ./build.sh
+ $ sh build.sh
+
+The output of the build will be in build.log.
 
 To start continuum:
 
-  cd continuum-web/generated-runtime
-  ./bin/plexus.sh
+ $ cd continuum-web/generated-runtime
+ $ ./bin/plexus.sh
 
 To access continuum's web site, go to:
 
   http://localhost:8080/continuumweb/servlet/continuum
-
-
--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ continuum-web/pom.xml,\
 continuum-xmlrpc/pom.xml
 
 m2 -N install "$@"
-m2 -r -Dmaven.reactor.includes=*/pom.xml clean:clean "$@"
-m2 -r -Dmaven.reactor.includes="$includes" install "$@"
+m2 -r -Dmaven.reactor.includes=*/pom.xml clean:clean "$@" 2>&1 | tee build.log
+m2 -r -Dmaven.reactor.includes="$includes" install "$@" 2>&1 | tee -a build.log
 
-( cd continuum-plexus-application && sh build.sh "$@" )
+( cd continuum-plexus-application && sh build.sh "$@" 2>&1 | tee -a ../build.log)
