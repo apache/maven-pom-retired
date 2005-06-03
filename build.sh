5099d4d596faaa5054dd4c343e58bff936accf7a
Trygve Laugstol <trygvis@apache.org>
2005-06-03 15:01:57 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@179811 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -11,8 +11,9 @@ continuum-model/pom.xml,\
 continuum-web/pom.xml,\
 continuum-xmlrpc/pom.xml
 
-m2 -N install "$@"
-m2 -r -Dmaven.reactor.includes=*/pom.xml clean:clean "$@" 2>&1 | tee build.log
-m2 -r -Dmaven.reactor.includes="$includes" install "$@" 2>&1 | tee -a build.log
-
-( cd continuum-plexus-application && sh build.sh "$@" 2>&1 | tee -a ../build.log)
+(
+  m2 -N install "$@"
+  m2 -r -Dmaven.reactor.includes=*/pom.xml clean:clean "$@"
+  m2 -r -Dmaven.reactor.includes="$includes" install "$@"
+  (cd continuum-plexus-application && sh build.sh "$@")
+) 2>&1 | tee -a ../build.log
