78c7365a8ae1dc5b0a59bbdacd7ed107367fc521
Jason van Zyl <jvanzyl@apache.org>
2005-06-20 06:43:51 +0000

    o need to use -D settings for the time being but this works and the POM   has commented out values for now so we can generate the runtime using m2   trunk.
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@191401 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-plexus-application/build.sh
+++ continuum-plexus-application/build.sh
@@ -1,1 +1,8 @@
-m2 -o clean:clean package plexus:bundle-application plexus:test-runtime "$@"
+m2 \
+   -DapplicationConfiguration=src/conf/application.xml \
+   -DconfigurationsDirectory=src/conf \
+   -DconfigurationProperties=app.properties \
+   -DapplicationName=continuum  \
+   -DruntimeConfiguration=src/test/conf/test-runtime-configuration.xml \
+   -DruntimeConfigurationProperties=app.properties \
+   clean:clean plexus:app plexus:bundle-application plexus:test-runtime
