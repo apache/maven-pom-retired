e9915bcf2a6fd172b0084a2bc9c49b46da41c499
Trygve Laugstol <trygvis@apache.org>
2005-07-01 11:56:01 +0000

    o Adding my own experiments. They probably won't work for you.
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@208750 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-plexus-application/plexus-sablevm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+PWD=`pwd`
+
+GNU_CLASSPATH=$HOME/opt/classpath-HEAD/share/classpath/glibj.zip
+JAVA=$HOME/opt/sablevm-trunk/bin/sablevm
+
+$JAVA \
+    -Xbootclasspath $GNU_CLASSPATH \
+    -classpath $PWD/target/plexus-test-runtime/core/boot/classworlds-1.1-alpha-1.jar \
+    -Dclassworlds.conf=$PWD/target/plexus-test-runtime/conf/classworlds.conf \
+    -Dplexus.core=$PWD/target/plexus-test-runtime/core \
+    -Djava.io.tmpdir=$PWD/target/plexus-test-runtime/temp \
+    -Dplexus.home=$PWD/target/plexus-test-runtime \
+    org.codehaus.classworlds.Launcher $PWD/target/plexus-test-runtime/conf/plexus.xml
