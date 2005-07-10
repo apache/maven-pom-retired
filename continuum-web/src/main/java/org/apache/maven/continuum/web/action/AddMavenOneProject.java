207c310c6771a5fb44a5d9c805bab2bb636ca23b
Jason van Zyl <jvanzyl@apache.org>
2005-07-10 21:43:12 +0000

    adding better validation
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@210045 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-web/src/main/java/org/apache/maven/continuum/web/action/AddMavenOneProject.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2005 Your Corporation. All Rights Reserved.
+ */
+package org.apache.maven.continuum.web.action;
+
+import org.apache.maven.continuum.Continuum;
+import org.apache.maven.continuum.project.builder.ContinuumProjectBuildingResult;
+import org.codehaus.plexus.formica.Form;
+import org.codehaus.plexus.formica.action.AbstractEntityAction;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
+
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id:$
+ */
+public class AddMavenOneProject
+    extends AbstractEntityAction
+    implements Contextualizable
+{
+    protected void uponSuccessfulValidation( Form form, String entityId, Map parameters )
+        throws Exception
+    {
+        Continuum continuum = (Continuum) container.lookup( Continuum.ROLE );
+
+        ContinuumProjectBuildingResult result = continuum.addMavenOneProject( (String) parameters.get( "m1PomUrl" ) );
+
+        if ( result.getWarnings().size() > 0 )
+        {
+            setResultMessages( result.getWarnings(), parameters );
+        }
+    }
+}
---
+++ continuum-web/src/main/java/org/apache/maven/continuum/web/action/AddMavenTwoProject.java
@@ -0,0 +1,31 @@
+package org.apache.maven.continuum.web.action;
+
+import org.apache.maven.continuum.Continuum;
+import org.apache.maven.continuum.project.builder.ContinuumProjectBuildingResult;
+import org.codehaus.plexus.formica.Form;
+import org.codehaus.plexus.formica.action.AbstractEntityAction;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
+
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id:$
+ */
+public class AddMavenTwoProject
+    extends AbstractEntityAction
+    implements Contextualizable
+{
+    protected void uponSuccessfulValidation( Form form, String entityId, Map parameters )
+        throws Exception
+    {
+        Continuum continuum = (Continuum) container.lookup( Continuum.ROLE );
+
+        ContinuumProjectBuildingResult result = continuum.addMavenTwoProject( (String) parameters.get( "m2PomUrl" ) );
+
+        if ( result.getWarnings().size() > 0 )
+        {
+            setResultMessages( result.getWarnings(), parameters );
+        }
+    }
+}
