40d1d930e462c7ba3770a6d1945f57d41da7961e
Jason van Zyl <jvanzyl@apache.org>
2005-07-19 12:41:02 +0000

    o adding web tools o adding schedule form definition
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@219674 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-web/src/main/java/org/apache/maven/continuum/web/tool/FormDataTool.java
@@ -0,0 +1,31 @@
+package org.apache.maven.continuum.web.tool;
+
+import org.codehaus.plexus.util.StringUtils;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id: CssTool.java 209843 2005-07-08 17:13:22Z evenisse $
+ */
+public class FormDataTool
+{
+    public List pulldown( String data )
+    {
+        List list = new ArrayList();
+
+        String[] items = StringUtils.split( data, "," );
+
+        for ( int i = 0; i < items.length; i++ )
+        {
+            String[] s = StringUtils.split( items[i], "=" );
+
+            Item item = new Item( s[0], s[1] );
+
+            list.add( item );
+        }
+
+        return list;
+    }
+}
