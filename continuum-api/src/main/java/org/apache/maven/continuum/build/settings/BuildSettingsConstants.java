03621cee0e31763d1fa56e6b665a8a6e6c67d92a
Jason van Zyl <jvanzyl@apache.org>
2005-07-21 15:42:30 +0000

    adding build settings constants
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@220116 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-api/src/main/java/org/apache/maven/continuum/build/settings/BuildSettingsConstants.java
@@ -0,0 +1,53 @@
+package org.apache.maven.continuum.build.settings;
+
+/*
+ * Copyright 2004-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id:$
+ */
+public class BuildSettingsConstants
+{
+    // ----------------------------------------------------------------------
+    // Notification
+    // ----------------------------------------------------------------------
+
+    public static final int NOTIFICATION_STRATEGY_ALWAYS = 0;
+
+    public static final int NOTIFICATION_STRATEGY_NEVER = 1;
+
+    public static final int NOTIFICATION_STRATEGY_ON_FAILURE = 2;
+
+    // ----------------------------------------------------------------------
+    // Labelling
+    // ----------------------------------------------------------------------
+
+    public static final int LABELLING_STRATEGY_ALWAYS = 0;
+
+    public static final int LABELLING_STRATEGY_NEVER = 0;
+
+    public static final int LABELLING_STRATEGY_ON_SUCCESS = 0;
+
+    // ----------------------------------------------------------------------
+    // Scm mode
+    // ----------------------------------------------------------------------
+
+    public static final int SCM_MODE_CLEAN_CHECKOUT = 0;
+
+    public static final int SCM_MODE_UPDATE = 1;
+
+}
