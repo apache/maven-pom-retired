63c44cb0cfebb71fdbd42e6f092d08ac92ba9112
Brett Leslie Porter <brett@apache.org>
2005-08-02 06:28:40 +0000

    more fixes for cygwin
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@226962 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-core-it/run.sh
+++ continuum-core-it/run.sh
@@ -7,8 +7,9 @@ set -x
 case "`uname`" in
   CYGWIN*) 
   # required for the integration tests - would be better for them to download a distro, extract and run against it
+  [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath -ws $JAVA_HOME | sed 's#\\\\$##'`
   [ -n "$ANT_HOME" ] && ANT_HOME=`cygpath -w $ANT_HOME`
-  [ -n "$MAVEN_HOME" ] && MAVEN_HOME=`cygpath -w $MAVEN_HOME`
+  [ -n "$MAVEN_HOME" ] && MAVEN_HOME=`cygpath -ws $MAVEN_HOME | sed 's#\\\\$##'`
   [ -n "$M2_HOME" ] && M2_HOME=`cygpath -w $M2_HOME`
   ;;
 esac
