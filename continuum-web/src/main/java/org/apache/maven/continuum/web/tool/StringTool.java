e8851cd18d7cc090b05da27d539feeecdfcf2816
Emmanuel Venisse <evenisse@apache.org>
2005-10-31 16:28:45 +0000

    Add project name in delete project message
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@329848 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-web/src/main/java/org/apache/maven/continuum/web/tool/StringTool.java
@@ -0,0 +1,13 @@
+package org.apache.maven.continuum.web.tool;
+
+/**
+ * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
+ * @version $Id$
+ */
+public class StringTool
+{
+    public int toInt( String value )
+    {
+        return Integer.parseInt( value );
+    }
+}
