9ca28387dc02adaf8692eafc383b474dc3f64c34
Emmanuel Venisse <evenisse@apache.org>
2005-12-14 16:35:00 +0000

    o Load Continuum object at start o Auto-create database o Add logger
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@356800 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-webapp/src/main/java/org/apache/maven/continuum/web/action/SummaryAction.java
+++ continuum-webapp/src/main/java/org/apache/maven/continuum/web/action/SummaryAction.java
@@ -1,16 +1,34 @@
 package org.apache.maven.continuum.web.action;
 
+import org.apache.maven.continuum.Continuum;
+
 import com.opensymphony.xwork.ActionSupport;
 
-import java.util.List;
+import java.util.Collection;
 
 public class SummaryAction
     extends ActionSupport
 {
-    private List projects;
+    private Continuum continuum;
+    
+    private Collection projects;
 
     public String execute()
+        throws Exception
     {
+        try
+        {
+            projects = continuum.getProjects();
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
         return SUCCESS;
+    }
+
+    public Collection getProjects()
+    {
+        return projects;
     }
 }
