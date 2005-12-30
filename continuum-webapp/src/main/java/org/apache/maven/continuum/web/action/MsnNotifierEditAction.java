704b7b21fadd4dc4ee2f16c3757dd771e3039b7f
Emmanuel Venisse <evenisse@apache.org>
2005-12-30 16:20:40 +0000

    Add MSN screen
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@360115 13f79535-47bb-0310-9956-ffa450edef68

---
+++ continuum-webapp/src/main/java/org/apache/maven/continuum/web/action/MsnNotifierEditAction.java
@@ -0,0 +1,88 @@
+package org.apache.maven.continuum.web.action;
+
+/*
+ * Copyright 2004-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.continuum.model.project.ProjectNotifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:evenisse@apache.org">Emmanuel Venisse</a>
+ * @version $Id$
+ */
+public class MsnNotifierEditAction
+    extends AbstractNotifierEditAction
+{
+    private String login;
+
+    private String password;
+
+    private String address;
+
+    protected void initConfiguration( Map configuration )
+    {
+        login = (String) configuration.get( "login" );
+
+        password = (String) configuration.get( "password" );
+
+        address = (String) configuration.get( "address" );
+    }
+
+    protected void setNotifierConfiguration( ProjectNotifier notifier )
+    {
+        HashMap configuration = new HashMap();
+
+        configuration.put( "login", login );
+
+        configuration.put( "password", password );
+
+        configuration.put( "address", address );
+
+        notifier.setConfiguration( configuration );
+    }
+
+    public String getLogin()
+    {
+        return login;
+    }
+
+    public void setLogin( String login )
+    {
+        this.login = login;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword( String password )
+    {
+        this.password = password;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress( String address )
+    {
+        this.address = address;
+    }
+}
--- continuum-webapp/src/main/webapp/notifierMsn.jsp
+++ continuum-webapp/src/main/webapp/notifierMsn.jsp
@@ -17,7 +17,19 @@
         </h3>
 
         <div class="axial">
-            MSN
+            <ww:form action="jabberNotifierEdit.action" method="post">
+                <ww:hidden name="notifierId"/>
+                <ww:hidden name="projectId"/>
+                <ww:hidden name="notifierType"/>
+                <ww:textfield label="%{getText('notifier.msn.login.label')}" name="login"/>
+                <ww:password label="%{getText('notifier.msn.password.label')}" name="password"/>
+                <ww:textfield label="%{getText('notifier.msn.address.label')}" name="address"/>
+                <ww:checkbox label="%{getText('notifier.event.sendOnSuccess')}" name="sendOnSuccess" value="sendOnSuccess" fieldValue="true"/>
+                <ww:checkbox label="%{getText('notifier.event.sendOnFailure')}" name="sendOnFailure" value="sendOnFailure" fieldValue="true"/>
+                <ww:checkbox label="%{getText('notifier.event.sendOnError')}" name="sendOnError" value="sendOnError" fieldValue="true"/>
+                <ww:checkbox label="%{getText('notifier.event.sendOnWarning')}" name="sendOnWarning" value="sendOnWarning" fieldValue="true"/>
+                <ww:submit value="%{getText('save')}"/>
+            </ww:form>
         </div>
       </div>
     </body>
