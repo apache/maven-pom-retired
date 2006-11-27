23f8833e1af8b2d2bc8fbc89170bbe00f0af4df2
Brett Leslie Porter <brett@apache.org>
2006-11-27 03:01:26 +0000

    move the sandbox out of trunk
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@479501 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-sandbox/db-tools/run-derby.sh
+++
@@ -1,8 +0,0 @@
-CP=
-CP=$CP:$HOME/.m2/repository/org/apache/incubator/derby/derby/10.0.2.1/derby-10.0.2.1.jar
-CP=$CP:$HOME/.m2/repository/org/apache/incubator/derby/derbynet/10.0.2.1/derbynet-10.0.2.1.jar
-
-mkdir -p target/derby
-java -cp $CP \
-         -Dderby.system.home=target/derby \
-         org.apache.derby.drda.NetworkServerControl start
