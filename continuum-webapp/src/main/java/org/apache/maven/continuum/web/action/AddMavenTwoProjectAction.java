9963f741865a50c3a32e9077261aff1aa9a88a5f
Emmanuel Venisse <evenisse@apache.org>
2007-01-04 09:57:59 +0000

    [CONTINUUM-1098] Add error message on uploaded pom with modules Submitted by: Franz Allan Valencia See
    git-svn-id: https://svn.apache.org/repos/asf/maven/continuum/trunk@492486 13f79535-47bb-0310-9956-ffa450edef68

--- continuum-webapp/src/main/java/org/apache/maven/continuum/web/action/AddMavenTwoProjectAction.java
+++ continuum-webapp/src/main/java/org/apache/maven/continuum/web/action/AddMavenTwoProjectAction.java
@@ -18,8 +18,15 @@ package org.apache.maven.continuum.web.action;
 
 import org.apache.maven.continuum.ContinuumException;
 import org.apache.maven.continuum.project.builder.ContinuumProjectBuildingResult;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Add a Maven 2 project to Continuum.
@@ -35,11 +42,59 @@ import java.io.File;
 public class AddMavenTwoProjectAction
     extends AddMavenProjectAction
 {
-
+    // TODO: remove this part once uploading of an m2 project with modules is supported ( CONTINUUM-1098 )
+    public static final String ERROR_UPLOADING_M2_PROJECT_WITH_MODULES = "add.m2.project.upload.modules.error";
+    public static final String ERROR_READING_POM_EXCEPTION_MESSAGE = "Error reading POM";
+    public static final String FILE_SCHEME = "file:/";
+    
     protected ContinuumProjectBuildingResult doExecute( String pomUrl, int selectedProjectGroup, boolean checkProtocol )
         throws ContinuumException
     {
-        return getContinuum().addMavenTwoProject( pomUrl, selectedProjectGroup, checkProtocol );
+        ContinuumProjectBuildingResult result = null;
+        
+        // TODO: remove this part once uploading of an m2 project with modules is supported ( CONTINUUM-1098 )
+        if ( checkProtocol == false )
+        {
+            MavenXpp3Reader m2pomReader = new MavenXpp3Reader();
+
+            try
+            {
+                String filePath = pomUrl;
+                if ( filePath.startsWith( FILE_SCHEME ) )
+                {
+                    filePath = filePath.substring( FILE_SCHEME.length() );
+                }
+
+                Model model = m2pomReader.read( new FileReader( filePath ) );
+
+                List modules = model.getModules();
+
+                if ( modules != null && modules.size() != 0 )
+                {
+                    result = new ContinuumProjectBuildingResult();
+                    result.addError( ERROR_UPLOADING_M2_PROJECT_WITH_MODULES );
+                }
+            }
+            catch ( FileNotFoundException e )
+            {
+                throw new ContinuumException( ERROR_READING_POM_EXCEPTION_MESSAGE, e );
+            }
+            catch ( IOException e )
+            {
+                throw new ContinuumException( ERROR_READING_POM_EXCEPTION_MESSAGE, e );
+           }
+            catch ( XmlPullParserException e )
+            {
+                throw new ContinuumException( ERROR_READING_POM_EXCEPTION_MESSAGE, e );
+            }
+        }
+
+        if ( result == null )
+        {
+            result = getContinuum().addMavenTwoProject( pomUrl, selectedProjectGroup, checkProtocol );
+        }
+        
+        return result;
     }
 
     public String doDefault()
