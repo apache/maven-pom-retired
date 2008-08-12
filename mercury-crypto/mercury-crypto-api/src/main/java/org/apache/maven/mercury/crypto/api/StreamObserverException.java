8260df70f7f802d2aa398b6b08d8addc35987f94
Oleg Gusakov <ogusakov@apache.org>
2008-08-12 00:07:23 +0000

    implemented PGP signatures, changed StreamVerifier modus operandi to get initialized with signature upfront as crypto signature needs that. Changed http client to follow this new paradigm
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/mercury@684995 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mercury-crypto/mercury-crypto-api/src/main/java/org/apache/maven/mercury/crypto/api/StreamObserverException.java
@@ -0,0 +1,54 @@
+package org.apache.maven.mercury.crypto.api;
+
+/**
+ *
+ *
+ * @author Oleg Gusakov
+ * @version $Id$
+ *
+ */
+public class StreamObserverException
+    extends Exception
+{
+
+  /**
+   * 
+   */
+  public StreamObserverException()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   */
+  public StreamObserverException(
+      String message )
+  {
+    super( message );
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param cause
+   */
+  public StreamObserverException(
+      Throwable cause )
+  {
+    super( cause );
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   * @param cause
+   */
+  public StreamObserverException(
+      String message,
+      Throwable cause )
+  {
+    super( message, cause );
+    // TODO Auto-generated constructor stub
+  }
+
+}
