84e585719e217de1189728e7086c618586abc1c4
Oleg Gusakov <ogusakov@apache.org>
2008-09-09 21:36:46 +0000

    it's start Nexus once with soft restarts between tests. Auth still crashes, but works fine with a standalone Nexus
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/mercury@693627 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mercury-it/src/test/java/org/apache/maven/mercury/repository/tests/Nexus.java
@@ -0,0 +1,126 @@
+package org.apache.maven.mercury.repository.tests;
+
+import java.net.ConnectException;
+
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.sonatype.appbooter.ForkedAppBooter;
+import org.sonatype.appbooter.ctl.AppBooterServiceException;
+import org.sonatype.nexus.client.NexusClient;
+import org.sonatype.nexus.client.rest.NexusRestClient;
+
+/**
+ *
+ *
+ * @author Oleg Gusakov
+ * @version $Id$
+ *
+ */
+public class Nexus
+{
+  public static final String TEST_NEXUS_ROLE = ForkedAppBooter.ROLE;
+  public static final String TEST_NEXUS_HINT = "NexusForkedAppBooter";
+
+  public static String nexusTestUrl  = "http://localhost:8091/nexus";
+  public static String nexusTestUser = "admin";
+  public static String nexusTestPass = "admin123";
+  
+  private static ForkedAppBooter nexusForkedAppBooter;
+  //------------------------------------------------------------------------------
+  private static void checkAppBooter( final PlexusContainer plexus )
+  throws AppBooterServiceException, ComponentLookupException
+  {
+    if( nexusForkedAppBooter != null )
+      return;
+    
+    nexusForkedAppBooter = (ForkedAppBooter)plexus.lookup( TEST_NEXUS_ROLE, TEST_NEXUS_HINT  );
+      
+    nexusForkedAppBooter.start();
+
+    Runtime.getRuntime().addShutdownHook
+    (
+        new Thread()
+        {
+          PlexusContainer plx = plexus;
+          private ForkedAppBooter nexusFAB;
+          
+          public void run()
+          {
+            try
+            {
+              nexusFAB = (ForkedAppBooter)plx.lookup( TEST_NEXUS_ROLE, TEST_NEXUS_HINT  );
+              nexusFAB.shutdown();
+            }
+            catch( Exception e )
+            {
+              e.printStackTrace();
+              return;
+            }
+          }
+        }
+    );
+  }
+  //------------------------------------------------------------------------------
+  public static void start( PlexusContainer plexus )
+  throws Exception
+  {
+    checkAppBooter( plexus );
+
+    NexusClient client = new NexusRestClient();
+    
+    client.connect( nexusTestUrl, nexusTestUser, nexusTestPass );
+    
+    try
+    {
+      client.startNexus();
+      Thread.sleep( 3000L );
+    }
+    catch( Exception any ) {}
+    
+    if( !client.isNexusStarted( true ) )
+    {
+      throw new Exception("Cannot start Nexus");
+    }
+    
+    client.disconnect();
+  }
+  //------------------------------------------------------------------------------
+  public static void stop()
+  throws Exception
+  {
+    NexusClient client = new NexusRestClient();
+    
+    client.connect( nexusTestUrl, nexusTestUser, nexusTestPass );
+
+    try
+    {
+      if( client.isNexusStarted( false ) )
+      {
+        client.stopNexus();
+      }
+    }
+    catch( Exception any ) {}
+
+    client.disconnect();
+    
+    Thread.sleep( 3000L );
+  }
+  //------------------------------------------------------------------------------
+  public static void shutdown( PlexusContainer plexus )
+  throws Exception
+  {
+    
+    if( nexusForkedAppBooter == null )
+      nexusForkedAppBooter = (ForkedAppBooter)plexus.lookup( TEST_NEXUS_ROLE, TEST_NEXUS_HINT  );
+      
+    try // if it was running before
+    {
+      nexusForkedAppBooter.shutdown();
+      Thread.sleep( 3000L );
+    }
+    catch( Exception e ) {}
+    
+  }
+  //------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------
+}
