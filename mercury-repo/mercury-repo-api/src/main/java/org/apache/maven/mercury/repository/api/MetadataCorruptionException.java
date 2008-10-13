6b9db58c5ad00bb7fe008ed62ebf1af1568b2f54
Oleg Gusakov <ogusakov@apache.org>
2008-10-13 17:51:26 +0000

    forgot to add the exception
    git-svn-id: https://svn.apache.org/repos/asf/maven/mercury/trunk@704184 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mercury-repo/mercury-repo-api/src/main/java/org/apache/maven/mercury/repository/api/MetadataCorruptionException.java
@@ -0,0 +1,54 @@
+package org.apache.maven.mercury.repository.api;
+
+/**
+ *
+ *
+ * @author Oleg Gusakov
+ * @version $Id$
+ *
+ */
+public class MetadataCorruptionException
+    extends Exception
+{
+
+  /**
+   * 
+   */
+  public MetadataCorruptionException()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   */
+  public MetadataCorruptionException(
+      String message )
+  {
+    super( message );
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param cause
+   */
+  public MetadataCorruptionException(
+      Throwable cause )
+  {
+    super( cause );
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   * @param cause
+   */
+  public MetadataCorruptionException(
+      String message,
+      Throwable cause )
+  {
+    super( message, cause );
+    // TODO Auto-generated constructor stub
+  }
+
+}
